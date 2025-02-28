34aacd30ceda76fb88e08edcb08c545344a22d93
curdicu <curdicu@gmail.com>
2025-03-01 01:34:39 +0800

    *

--- Package.swift
+++ Package.swift
@@ -3,49 +3,30 @@
 import PackageDescription
 
 let package = Package(
-    name: "BitcoinCore",
+    name: "BitcoinKit",
     platforms: [
         .iOS(.v13),
     ],
     products: [
         .library(
-            name: "BitcoinCore",
-            targets: ["BitcoinCore"]
+            name: "BitcoinKit",
+            targets: ["BitcoinKit"]
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/attaswift/BigInt.git", .upToNextMajor(from: "5.0.0")),
-        .package(url: "https://github.com/Brightify/Cuckoo.git", .upToNextMajor(from: "1.5.0")),
-        .package(url: "https://github.com/groue/GRDB.swift.git", .upToNextMajor(from: "6.0.0")),
-        .package(url: "https://github.com/Quick/Nimble.git", from: "10.0.0"),
-        .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "5.0.1")),
-
-        .package(url: "https://github.com/horizontalsystems/Checkpoints.git", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/horizontalsystems/BitcoinCore.Swift.git", .upToNextMajor(from: "3.0.0")),
+        .package(url: "https://github.com/horizontalsystems/Hodler.Swift.git", .upToNextMajor(from: "2.0.1")),
         .package(url: "https://github.com/horizontalsystems/HdWalletKit.Swift.git", .upToNextMajor(from: "1.2.1")),
-        .package(url: "https://github.com/horizontalsystems/HsCryptoKit.Swift.git", .upToNextMajor(from: "1.2.1")),
-        .package(url: "https://github.com/horizontalsystems/HsExtensions.Swift.git", .upToNextMajor(from: "1.0.6")),
-        .package(url: "https://github.com/horizontalsystems/HsToolKit.Swift.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/horizontalsystems/HsToolKit.Swift.git", .upToNextMajor(from: "2.0.5")),
     ],
     targets: [
         .target(
-            name: "BitcoinCore",
+            name: "BitcoinKit",
             dependencies: [
-                "BigInt",
-                "Checkpoints",
-                .product(name: "GRDB", package: "GRDB.swift"),
+                .product(name: "BitcoinCore", package: "BitcoinCore.Swift"),
+                .product(name: "Hodler", package: "Hodler.Swift"),
                 .product(name: "HdWalletKit", package: "HdWalletKit.Swift"),
-                .product(name: "HsCryptoKit", package: "HsCryptoKit.Swift"),
-                .product(name: "HsExtensions", package: "HsExtensions.Swift"),
                 .product(name: "HsToolKit", package: "HsToolKit.Swift"),
-            ]
-        ),
-        .testTarget(
-            name: "BitcoinCoreTests",
-            dependencies: [
-                "BitcoinCore",
-                "Cuckoo",
-                "Nimble",
-                "Quick",
             ]
         ),
     ]
